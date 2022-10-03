bc10c9af90bb88fd8d89806b095d6d7ffc46af97
Ben <ben.stimpson@outlook.com>
2022-10-03 18:13:36 +0100

    Updated: SignUp page calling service worker - needs to set redux state

--- client/src/utils/SignUpServices.tsx
+++ client/src/utils/SignUpServices.tsx
@@ -1,11 +1,17 @@
-const createUser = async (email: string, password: string) => {
+export const createUser = async (
+	email: FormDataEntryValue | null,
+	password: FormDataEntryValue | null
+) => {
 	try {
-		await fetch('http://localhost:3030/register', {
+		const response = await fetch('http://localhost:3030/register', {
 			method: 'POST',
 			headers: { 'Content-type': 'application/json' },
 			body: JSON.stringify({ email, password }),
 		});
+		const data = await response.json();
+		return data;
 	} catch (err) {
 		console.log('SignUp CreateUser error:', err);
+		return new Error('Failed to create user');
 	}
 };
