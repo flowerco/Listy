d4b4772ebf7f2b6de7f52203af25023b66b23fbd
Ben <ben.stimpson@outlook.com>
2022-10-05 18:34:55 +0100

    Added: Testing with typescript

---
+++ client/src/tests/LoginPage.test.tsx
@@ -0,0 +1,8 @@
+import { jest } from '@jest/globals';
+import { render, fireEvent } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import LoginPage from '../components/pages/LoginPage';
+
+describe('<LoginForm />', () => {
+	test('renders without crashing', () => {});
+});
